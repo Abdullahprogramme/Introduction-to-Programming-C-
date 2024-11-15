a72b32f346122b844276b52c0b35000f5cc555f3
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-11-15 15:02:08 +0500

    Update Privileges.cpp

--- Pointers/Privileges.cpp
+++ Pointers/Privileges.cpp
@@ -1,57 +1,54 @@
-// All four methods are used to set the privileges of the current process.
-// Making four functions to set the privileges of the current process.
-
 #include <iostream>
 
 using namespace std;
 
-void nonconst_pointer_nonconst_data(int* ptr);
-void const_pointer_nonconst_data(int const* ptr);
-void nonconst_pointer_const_data(int* const ptr);
-void const_pointer_const_data(int const* const ptr);
+// Function declarations with corrected and clear naming
+void nonconst_pointer_nonconst_data(int* ptr);         // Non-const pointer to non-const data
+void nonconst_pointer_const_data(int const* ptr);      // Non-const pointer to const data
+void const_pointer_nonconst_data(int* const ptr);      // Const pointer to non-const data
+void const_pointer_const_data(int const* const ptr);   // Const pointer to const data
 
 int main() {
     int x = 10;
 
-    // creating pointers
-    int* ptr1 = &x;
-    int const* ptr2 = &x;
-    int* const ptr3 = &x;
-    int const* const ptr4 = &x;
+    // Creating pointers
+    int* ptr1 = &x;           // Non-const pointer to non-const data
+    int const* ptr2 = &x;     // Non-const pointer to const data
+    int* const ptr3 = &x;     // Const pointer to non-const data
+    int const* const ptr4 = &x; // Const pointer to const data
 
-    // calling functions
+    // Calling functions
     nonconst_pointer_nonconst_data(ptr1);
-    const_pointer_nonconst_data(ptr2);
-    nonconst_pointer_const_data(ptr3);
+    nonconst_pointer_const_data(ptr2);
+    const_pointer_nonconst_data(ptr3);
     const_pointer_const_data(ptr4);
 
     return 0;
 }
 
+// Function definitions
 void nonconst_pointer_nonconst_data(int* ptr) {
-    *ptr = 20; // can change the data
-    int y = 20;
-    ptr = &y; // can change the pointer to point to another memory location
-    cout << "nonconst_pointer_nonconst_data: " << *ptr << endl; // can read the data
+    *ptr = 20; // Can modify the data
+    int y = 30;
+    ptr = &y;  // Can change the pointer to point to another memory location
+    cout << "nonconst_pointer_nonconst_data: " << *ptr << endl;
 }
 
-void const_pointer_nonconst_data(int const* ptr) {
-    // *ptr = 20; // cannot change the data
-    int y = 20;
-    ptr = &y; // can change the pointer to point to another memory location
-    cout << "const_pointer_nonconst_data: " << *ptr << endl; // can read the data
+void nonconst_pointer_const_data(int const* ptr) {
+    // *ptr = 20; // Cannot modify the data (commented out because it's invalid)
+    int y = 40;
+    ptr = &y;  // Can change the pointer to point to another memory location
+    cout << "nonconst_pointer_const_data: " << *ptr << endl;
 }
 
-void nonconst_pointer_const_data(int* const ptr) {
-    *ptr = 20; // can change the data
-    // int y = 20;
-    // ptr = &y; // cannot change the pointer to point to another memory location
-    cout << "nonconst_pointer_const_data: " << *ptr << endl; // can read the data
+void const_pointer_nonconst_data(int* const ptr) {
+    *ptr = 50; // Can modify the data
+    // ptr = &x; // Cannot change the pointer to point to another memory location (commented out because it's invalid)
+    cout << "const_pointer_nonconst_data: " << *ptr << endl;
 }
 
 void const_pointer_const_data(int const* const ptr) {
-    // *ptr = 20; // cannot change the data
-    // int y = 20;
-    // ptr = &y; // cannot change the pointer to point to another memory location
-    cout << "const_pointer_const_data: " << *ptr << endl; // can read the data
+    // *ptr = 20; // Cannot modify the data (commented out because it's invalid)
+    // ptr = &x; // Cannot change the pointer to point to another memory location (commented out because it's invalid)
+    cout << "const_pointer_const_data: " << *ptr << endl;
 }
